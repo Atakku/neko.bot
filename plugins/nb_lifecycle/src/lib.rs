940636654c00881818ddb0645c3e9a743a2db576
Atakku <atakkudev@gmail.com>
2023-06-01 22:12:02 +0200

    Ever so closer to finishing refactoring

--- plugins/nb_lifecycle/src/lib.rs
+++ plugins/nb_lifecycle/src/lib.rs
@@ -4,11 +4,28 @@
 
 #![feature(async_fn_in_trait)]
 
-use futures::future::{join_all, BoxFuture, LocalBoxFuture};
+use futures::future::{BoxFuture, LocalBoxFuture};
 use futures_locks::RwLock;
-use nbf::{Framework, Plugin, Res, SharedData, State, R};
+use nbf::{Res, SharedData, State, R};
 
 pub type ArcState = RwLock<State>;
+
+pub type HR = LocalBoxFuture<'static, R>;
+pub type Hook = fn(ArcState) -> HR;
+
+pub type MHR = BoxFuture<'static, R>;
+pub type MainHook = fn(ArcState) -> MHR;
+
+#[cfg(feature = "framework")]
+mod framework;
+#[cfg(feature = "framework")]
+pub use framework::*;
+
+#[cfg(feature = "plugin")]
+mod plugin;
+#[cfg(feature = "plugin")]
+pub use plugin::*;
+
 
 pub trait ArcStateHelper {
   async fn put<T>(&self, t: T) -> R
@@ -34,87 +51,4 @@ impl ArcStateHelper for ArcState {
   where T: SharedData {
     self.write().await.take::<T>()
   }
-}
-
-pub type HR = LocalBoxFuture<'static, R>;
-pub type Hook = fn(ArcState) -> HR;
-
-pub type MHR = BoxFuture<'static, R>;
-pub type MainHook = fn(ArcState) -> MHR;
-
-pub struct LifecycleHooks {
-  pre: Vec<Hook>,
-  main: Vec<MainHook>,
-  post: Vec<Hook>,
-}
-
-impl Default for LifecycleHooks {
-  fn default() -> Self {
-    Self {
-      pre: Vec::new(),
-      main: Vec::new(),
-      post: Vec::new(),
-    }
-  }
-}
-
-pub struct LifecyclePlugin;
-
-impl Default for LifecyclePlugin {
-  fn default() -> Self {
-    Self {}
-  }
-}
-
-impl Plugin for LifecyclePlugin {
-  fn init(self, fw: &mut Framework) -> R {
-    fw.state.put(LifecycleHooks::default());
-    Ok(())
-  }
-}
-
-pub trait LifecycleFramework {
-  fn pre_hook(&mut self, hook: Hook) -> Res<&mut Framework>;
-  fn main_hook(&mut self, hook: MainHook) -> Res<&mut Framework>;
-  fn post_hook(&mut self, hook: Hook) -> Res<&mut Framework>;
-  async fn run(self) -> R;
-}
-
-impl LifecycleFramework for Framework {
-  fn pre_hook(&mut self, hook: Hook) -> Res<&mut Framework> {
-    self.state.borrow_mut::<LifecycleHooks>()?.pre.push(hook);
-    Ok(self)
-  }
-  fn main_hook(&mut self, hook: MainHook) -> Res<&mut Framework> {
-    self.state.borrow_mut::<LifecycleHooks>()?.main.push(hook);
-    Ok(self)
-  }
-  fn post_hook(&mut self, hook: Hook) -> Res<&mut Framework> {
-    self.state.borrow_mut::<LifecycleHooks>()?.post.push(hook);
-    Ok(self)
-  }
-
-  async fn run(self) -> R {
-    let mut state = self.state;
-    let hooks = state.take::<LifecycleHooks>()?;
-    let state = RwLock::new(state);
-    run_sequential(hooks.pre, &state).await?;
-    run_parallel(hooks.main, &state).await?;
-    run_sequential(hooks.post, &state).await?;
-    Ok(())
-  }
-}
-
-async fn run_sequential(hooks: Vec<Hook>, state: &ArcState) -> R {
-  for hook in hooks {
-    (hook)(state.clone()).await?;
-  }
-  Ok(())
-}
-
-async fn run_parallel(hooks: Vec<MainHook>, state: &ArcState) -> R {
-  for res in join_all(hooks.into_iter().map(|h| tokio::spawn((h)(state.clone())))).await {
-    res??
-  }
-  Ok(())
 }
