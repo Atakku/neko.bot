f90fe2b1775529437b8054b188656c6e4a306f49
Atakku <atakkudev@gmail.com>
2023-06-02 21:27:06 +0200

    Add type logging in sqlx

--- plugins/nb_sqlx/src/plugin.rs
+++ plugins/nb_sqlx/src/plugin.rs
@@ -2,6 +2,8 @@
 //
 // This project is dual licensed under MIT and Apache.
 
+use std::any::type_name;
+
 use nbf::{Framework, Plugin, R};
 use sqlx::{pool::PoolOptions, Database};
 
@@ -25,7 +27,7 @@ where T: Database
 
 impl<T: sqlx::Database> Plugin for SqlxPlugin<T> {
   fn init(self, fw: &mut Framework) -> R {
-    log::trace!("SqlxPlugin::init()");
+    log::trace!("SqlxPlugin<{}>::init()", type_name::<T>());
     fw.state.put(self.opts.connect_lazy(&self.db_url)?);
     Ok(())
   }
